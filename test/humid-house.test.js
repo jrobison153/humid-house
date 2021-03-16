35fba8bab6754351ac81eea1477dc4ed92d0434e
Justin Robison <justin.robison@libertymutual.com>
2021-03-16 05:51:08 -0400

    thing associated to principal

--- test/humid-house.test.js
+++ test/humid-house.test.js
@@ -1,8 +1,13 @@
 /* eslint-disable require-jsdoc */
 const awsAssert = require('@aws-cdk/assert');
 const humidHouseStack = require('../lib/bootstrap-stack');
-const {RASPBERRY_PI_S01_CERT_ID, RASPBERRY_PI_S01} = require('../lib/humid-house-stack');
 const {
+  RASPBERRY_PI_S01_CERT_ID,
+  RASPBERRY_PI_S01,
+  RASPBERRY_PI_S01_PRINCIPAL_ATTACHEMENT_ID,
+} = require('../lib/humid-house-stack');
+const {
+  beASupersetOfTemplate,
   countResources,
   haveResource,
   haveResourceLike,
@@ -17,6 +22,7 @@ const STACK_ID = 'MyTestStack';
 
 let defaultCsrAdapter;
 let cacheStub;
+let stack;
 
 beforeEach(async () => {
 
@@ -25,6 +31,8 @@ beforeEach(async () => {
   defaultCsrAdapter = cachedCsr(cacheStub);
 
   csrFactory.setDefaultCsrAdapter(defaultCsrAdapter);
+
+  stack = await humidHouseStack(STACK_ID);
 });
 
 afterEach(() => {
@@ -36,21 +44,15 @@ describe('When generating the stack', () => {
 
   test('Then the IoT thing exists', async () => {
 
-    const stack = await humidHouseStack(STACK_ID);
-
     awsExpect(stack).to(haveResource('AWS::IoT::Thing'));
   });
 
   test('Then there is only one IoT thing', async () => {
 
-    const stack = await humidHouseStack(STACK_ID);
-
     awsExpect(stack).to(countResources('AWS::IoT::Thing'), 1);
   });
 
   test('Then there is an IoT thing with the correct id', async () => {
-
-    const stack = await humidHouseStack(STACK_ID);
 
     const synthedStack = SynthUtils.toCloudFormation(stack);
 
@@ -59,16 +61,12 @@ describe('When generating the stack', () => {
 
   test('Then the IoT thing has its name set', async () => {
 
-    const stack = await humidHouseStack(STACK_ID);
-
     awsExpect(stack).to(haveResourceLike('AWS::IoT::Thing', {
       ThingName: RASPBERRY_PI_S01,
     }));
   });
 
   test('Then the IoT thing has the location attribute set', async () => {
-
-    const stack = await humidHouseStack(STACK_ID);
 
     awsExpect(stack).to(haveResourceLike('AWS::IoT::Thing', {
       AttributePayload: {
@@ -81,8 +79,6 @@ describe('When generating the stack', () => {
 
   test('Then certificate assigned with active status', async () => {
 
-    const stack = await humidHouseStack(STACK_ID);
-
     awsExpect(stack).to(haveResourceLike('AWS::IoT::Certificate', {
       Status: 'ACTIVE',
     }));
@@ -91,10 +87,6 @@ describe('When generating the stack', () => {
   describe('And the CSR has not been previously cached', () => {
 
     test('Then certificate assigned with new CSR', async () => {
-
-      cacheStub.clearCache();
-
-      const stack = await humidHouseStack(STACK_ID);
 
       const synthedStack = SynthUtils.toCloudFormation(stack);
 
@@ -106,8 +98,6 @@ describe('When generating the stack', () => {
   describe('And the stack has been previously generated', () => {
 
     test('Then the same CSR is used from cache on the second run', async () => {
-
-      cacheStub.clearCache();
 
       const firstStack = await humidHouseStack(STACK_ID);
 
@@ -125,6 +115,23 @@ describe('When generating the stack', () => {
       expect(initiallyCreatedCsr).toEqual(cachedHitCsr);
     });
   });
-});
 
-// ============ Util Functions ================
+  test('Then the Thing is attached to the x.509 principal', () => {
+
+    awsExpect(stack).to(beASupersetOfTemplate({
+      Resources: {
+        [RASPBERRY_PI_S01_PRINCIPAL_ATTACHEMENT_ID]: {
+          Type: 'AWS::IoT::ThingPrincipalAttachment',
+          Properties: {
+            ThingName: {
+              Ref: RASPBERRY_PI_S01,
+            },
+            Principal: {
+              Ref: RASPBERRY_PI_S01_CERT_ID,
+            },
+          },
+        },
+      },
+    }));
+  });
+});
